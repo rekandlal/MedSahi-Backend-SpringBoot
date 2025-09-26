1bfd1bf3a7bad91d73dafd3945eaead4db2871bc
demo1 <rekandlal11@gmail.com>
2025-09-26 18:13:35 +0530

    add all files

---
+++ src/main/java/com/demo/medsahispringboot/MedSahiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.medsahispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSahiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedSahiSpringBootApplication.class, args);
+    }
+
+}
